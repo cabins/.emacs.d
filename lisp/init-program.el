24f3ff7f9b6a00076f3cdb923913ab4d28b02e0d
Cabins <kong_lingcun@163.com>
2020-01-10 15:43:01 +0800

    update lsp and golang settings

--- lisp/init-program.el
+++ lisp/init-program.el
@@ -2,6 +2,10 @@
 ;;; Commentary:
 ;;; Code:
 
+;;; Basic Programming Style
+(setq indent-tabs-mode t)
+(setq tab-width 4)
+
 ;;; ================================================================================
 ;;;                          Common LSP settings
 ;;; ================================================================================
@@ -32,20 +36,19 @@
 
 ;;; ------------------------------ Go ------------------------------
 ;; export GO111MODULE=on; go get -v golang.org/x/tools/gopls@latest
-(use-package go-mode
-  :mode (("\\.go'" . go-mode))
-  :hook (go-mode . lsp)
-  :config
-  (setq indent-tabs-mode t)
-  (setq tab-width 4)
-  )
-
-;; Set up before-save hooks to format buffer and add/delete imports.
-;; Make sure you don't have other gofmt/goimports hooks enabled.
 (defun lsp-go-install-save-hooks ()
   (add-hook 'before-save-hook #'lsp-format-buffer t t)
   (add-hook 'before-save-hook #'lsp-organize-imports t t))
-(add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
+
+(use-package go-mode
+  :hook (go-mode . lsp-go-install-save-hooks))
+
+;; Set up before-save hooks to format buffer and add/delete imports.
+;; Make sure you don't have other gofmt/goimports hooks enabled.
+;; (defun lsp-go-install-save-hooks ()
+;;   (add-hook 'before-save-hook #'lsp-format-buffer t t)
+;;   (add-hook 'before-save-hook #'lsp-organize-imports t t))
+;; (add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
 
 (provide 'init-program)
 
