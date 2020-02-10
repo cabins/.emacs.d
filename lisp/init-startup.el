2b05f4e2becb0fb0fea1dfa52085e1aaa4f82428
Cabins <kong_lingcun@163.com>
2020-02-10 08:51:32 +0800

    modify the garbage collection thresholds

--- lisp/init-startup.el
+++ lisp/init-startup.el
@@ -13,12 +13,7 @@
 (setq make-backup-files nil)
 (setq auto-save-default nil)
 
-;; Adjust garbage collection thresholds during startup, and thereafter -from purcell config
-;; (let ((normal-gc-cons-threshold (* 20 1024 1024))
-;;       (init-gc-cons-threshold (* 128 1024 1024)))
-;;   (setq gc-cons-threshold init-gc-cons-threshold)
-;;   (add-hook 'emacs-startup-hook
-;; 	    (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
+;; Adjust garbage collection thresholds during startup
 (setq gc-cons-threshold most-positive-fixnum)
 
 ;; Settings for GUI misc
