281e31864945b80b4033ea4601af0dcda2ff8682
11069956 <11069956@vivo.xyz>
2020-02-24 10:59:17 +0800

    maximize window before initialize emacs NOTE: if you do NOT like this, remember to comment this commit

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -6,12 +6,12 @@
 ;; Initialize the GUI size & fonts
 ;; Settings for GUI mode
 
-(when (display-graphic-p)
-  ;; Init GUI size as maximized mode
-  (set-frame-parameter nil 'fullscreen 'maximized)
-    ;;; If you don't want maximized window, you can use the next line to define a customized size
-  ;; (setq default-frame-alist '((width . 150) (height . 35)))
-  )
+;; (when (display-graphic-p)
+;;   ;; Init GUI size as maximized mode
+;;   (set-frame-parameter nil 'fullscreen 'maximized)
+;;     ;;; If you don't want maximized window, you can use the next line to define a customized size
+;;   ;; (setq default-frame-alist '((width . 150) (height . 35)))
+;;   )
 (use-package smart-mode-line-powerline-theme)
 
 (when (or *is-mac* *is-linux*)
