05b8719105d6c7ce6e2d3a9e53f0f287947499cb
11069956 <11069956@vivo.xyz>
2020-02-28 16:10:43 +0800

    update ui settings: customize fonts only when in graphic mode

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -2,22 +2,16 @@
 ;;; Commentary:
 ;;; Code:
 
-
 ;; Initialize the GUI size & fonts
 ;; Settings for GUI mode
-
-;; (when (display-graphic-p)
-;;   ;; Init GUI size as maximized mode
-;;   (set-frame-parameter nil 'fullscreen 'maximized)
-;;     ;;; If you don't want maximized window, you can use the next line to define a customized size
-;;   ;; (setq default-frame-alist '((width . 150) (height . 35)))
-;;   )
 (use-package smart-mode-line-powerline-theme)
 
 (when (or *is-mac* *is-linux*)
   (progn
     ;; Font settings
-    (set-face-attribute 'default nil :font "Source Code Pro for Powerline 11")
+    (when (display-graphic-p)
+      (set-face-attribute 'default nil :font "Source Code Pro for Powerline 11")
+      )
     ;; Settings for UI theme
     (use-package spacemacs-theme
       :init (load-theme 'spacemacs-dark t))
@@ -30,10 +24,12 @@
 (when *is-windows*
   (progn
     ;; Font settings
-    (set-face-attribute 'default nil :font "Microsoft Yahei Mono 9")
-    (dolist (charset '(kana han symbol cjk-misc bopomofo))
-      (set-fontset-font (frame-parameter nil 'font)
-                        charset (font-spec :family "Microsoft Yahei Mono" :size 9)))
+    (when (display-graphic-p)
+      (set-face-attribute 'default nil :font "Microsoft Yahei Mono 9")
+      (dolist (charset '(kana han symbol cjk-misc bopomofo))
+        (set-fontset-font (frame-parameter nil 'font)
+                          charset (font-spec :family "Microsoft Yahei Mono" :size 9)))
+      )
     (use-package smart-mode-line
       :init (setq sml/no-confirm-load-theme t
                   sml/theme 'light-powerline)
