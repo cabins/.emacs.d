798fdc57fd9889060b33913a8837965c67127633
Cabins <kong_lingcun@163.com>
2020-03-01 07:07:57 +0800

    fix: maximize window does NOT work on macOS

--- init.el
+++ init.el
@@ -6,15 +6,6 @@
 
 ;;; Code:
 
-;; I Like maximize the frame window
-;; Comment it if you do NOT like this.
-(when (display-graphic-p)
-  ;; Init GUI size as maximized mode
-  (set-frame-parameter nil 'fullscreen 'maximized)
-    ;;; If you don't want maximized window, you can use the next line to define a customized size
-  ;; (setq default-frame-alist '((width . 150) (height . 35)))
-  )
-
 ;; Settings for the lisp config path
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
