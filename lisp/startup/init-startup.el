9b43e133f4d5a0a3e9a103d9774199341ce80d11
11069956 <11069956@vivo.xyz>
2020-03-27 09:38:23 +0800

    fix font bug

--- lisp/startup/init-startup.el
+++ lisp/startup/init-startup.el
@@ -7,7 +7,8 @@
 
 ;; Settings for system encoding
 (prefer-coding-system 'utf-8)
-(setq locale-coding-system 'utf-8)
+;; (setq locale-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
 
 (set-language-environment 'utf-8)
 (set-default-coding-systems 'utf-8)
