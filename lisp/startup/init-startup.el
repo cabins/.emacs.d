27d412d5cab45a4a23f4856ca56f0e67fccef7da
11069956 <11069956@vivo.xyz>
2020-03-27 08:55:42 +0800

    update coding settings

--- lisp/startup/init-startup.el
+++ lisp/startup/init-startup.el
@@ -7,10 +7,16 @@
 
 ;; Settings for system encoding
 (prefer-coding-system 'utf-8)
+(setq locale-coding-system 'utf-8)
+
+(set-language-environment 'utf-8)
 (set-default-coding-systems 'utf-8)
-(set-terminal-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(set-clipboard-coding-system 'utf-8)
+(set-file-name-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
-(setq default-buffer-file-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
 
 ;; Settings for backup files
 (setq make-backup-files nil
