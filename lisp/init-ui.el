17b61f1a024276b4d6ffc45695d72818a17f689e
cabins <kong_lingcun@163.com>
2020-10-24 15:46:41 +0800

    update ui config

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -5,39 +5,24 @@
 
 ;;; Code:
 
-;; Settings for UI theme
-(use-package doom-themes
-  :init (load-theme 'doom-one t))
-
-(use-package smart-mode-line
-  :init
-  (setq sml/no-confirm-load-theme t
-	    sml/theme 'respectful)
-  (sml/setup))
+;; ;; Settings for UI theme
+;; (use-package doom-themes
+;;   :init (load-theme 'doom-one t))
 
 ;; Font settings
 (use-package emacs
   :when (display-graphic-p)
   :config
   (set-default 'cursor-type 'bar)
+  (setq-default scroll-up-aggressively 0.01
+                scroll-down-aggressively 0.01)
   (setq default-frame-alist '((width . 180) (height . 40)))
-  ;; (set-frame-parameter nil 'fullscreen 'maximized)
-  ;; Set fonts global
-
-  ;; (when *is-windows*
-  ;;   (setq face-font-rescale-alist '(("Microsoft Yahei Mono" . 1)))
-  ;;   (dolist (charset '(kana han symbol cjk-misc bopomofo))
-  ;;     (set-fontset-font (frame-parameter nil 'font) charset (font-spec :family "Microsoft Yahei Mono" :size 10.5))))
-  ;; (when *is-mac*
-  ;;   (set-face-attribute 'default nil :font "Monaco 11")
-  ;;   (dolist (charset '(kana han symbol cjk-misc bopomofo))
-  ;;     (set-fontset-font (frame-parameter nil 'font) charset (font-spec :family "华文细黑" :size 14.5)))))
-
-  )
-
-(use-package cnfonts
-  :init (cnfonts-enable)
-  )
+  (setq redisplay-dont-pause t
+        scroll-conservatively most-positive-fixnum
+        scroll-margin 1
+        scroll-step 1
+        scroll-preserve-screen-position 'always)
+)
 
 (provide 'init-ui)
 ;;; init-ui.el ends here
