ab5300d582162a6cd897798d524baa50b3f245b1
cabins <kong_lingcun@163.com>
2020-12-11 10:57:03 +0800

    fix highlight line bug

--- lisp/init-startup.el
+++ lisp/init-startup.el
@@ -45,11 +45,11 @@
 ;; blink the cursor
 (blink-cursor-mode 1)
 
+(global-hl-line-mode 1)
 ;; 高亮当前行，使用浅灰色背景条
 (set-face-background hl-line-face "#F2F2F2")
 ;; 高亮当前行，使用下划线
 ;; (set-face-underline-p 'highlight t)
-(global-hl-line-mode 1)
 
 (if *is-mac*
     (setq delete-by-moving-to-trash t))
