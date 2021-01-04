bb166d97456fa0250dd77a86777ae2a58b987275
cabins <kong_lingcun@163.com>
2021-01-04 14:26:28 +0800

    fix bugs

--- lisp/interactive-funcs.el
+++ lisp/interactive-funcs.el
@@ -24,7 +24,7 @@
   "Print the login user info as init message"
   (let ((prefix ";; Configuration by Cabins <github.com/cabins>.\n")
         (os-version (format ";; %20s: %S\n" "Operating System" system-type))
-        (user-names (format ";; %20s: %s\n" "User Name" (system-users)))
+        (user-names (format ";; %20s: %s\n" "Login User" (user-login-name)))
         (machine-name (format ";; %20s: %s\n" "Machine Name" (system-name)))
         (suffix ";; Enjoy!"))
     (concat prefix ";;\n" os-version user-names machine-name ";;\n" suffix)))
