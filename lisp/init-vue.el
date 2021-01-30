0a5095fcf752f21cc5d4327ec1072065316a7e2b
Cabins <kong_lingcun@163.com>
2021-01-30 10:43:20 +0800

    fix typo

--- lisp/init-vue.el
+++ lisp/init-vue.el
@@ -5,7 +5,7 @@
 ;;; Code:
 
 (use-package vue-mode
-  ;; disable the ungly background color
+  ;; disable the ugly background color
   ;; https://github.com/AdamNiederer/vue-mode#how-do-i-disable-that-ugly-background-color
   :config (set-face-background 'mmm-default-submode-face nil))
 
