664c31130a69dc038eee3645ee203c80ee36b359
cabins <kong_lingcun@foxmail.com>
2021-08-25 20:11:36 +0800

    fix code format

--- lisp/init-lsp-language.el
+++ lisp/init-lsp-language.el
@@ -1,4 +1,3 @@
-
 ;;; init-lsp-language.el --- settings for languages of lsp mode
 ;;; Commentary: (c) Cabins, github.com/cabins/.emacs.d
 ;;; Code:
@@ -10,7 +9,6 @@
 
 ;;; the following settings sorted by the package name (alphabet order)
 ;; GO MODE
-;; ==============================
 (defvar go--tools '("golang.org/x/tools/cmd/goimports"
                     "github.com/go-delve/delve/cmd/dlv"
                     "github.com/josharian/impl"
@@ -27,20 +25,16 @@
   (use-package go-tag))
 
 ;; JSON MODE
-;; ==============================
 (use-package json-mode)
 
 ;; LISP MODE
-;; ==============================
 (use-package paredit
   :hook (emacs-lisp-mode . enable-paredit-mode))
 
 ;; MARKDOWN MODE
-;; ==============================
 (use-package markdown-mode)
 
 ;; PYTHON MODE
-;; ==============================
 (use-package python-mode
   :ensure nil
   :hook (python-mode . (lambda()(add-hook 'before-save-hook #'python-isort)))
@@ -52,26 +46,22 @@
   :args '("--stdout" "--atomic" "--profile=black" "-"))
 
 ;; RESTCLIENT - HTTP CLIENT
-;; ==============================
 (use-package restclient
   :mode ("\\.http\\'" . restclient-mode))
 
 ;; RUST MODE
-;; ==============================
 (use-package rust-mode
   :bind (:map rust-mode-map ("C-c C-c" . rust-run)) ;bind the rust-run key
   :config (setq indent-tabs-mode nil                ;rust use spaces instead of tab
                 rust-format-on-save t))
 
 ;; VUE.js MODE
-;; ==============================
 (use-package vue-mode
   ;; disable the ugly background color
-  ;; https://github.com/AdamNiederer/vue-mode#how-do-i-disable-that-ugly-background-color
+  ;; [refs] https://github.com/AdamNiederer/vue-mode#how-do-i-disable-that-ugly-background-color
   :config (set-face-background 'mmm-default-submode-face nil))
 
 ;; WEB MODE
-;; ==============================
 (use-package web-mode
   :mode ("\\.html\\'")
   :config
@@ -87,9 +77,7 @@
     :hook (web-mode css-mode)))
 
 ;; YAML MODE
-;; ==============================
 (use-package yaml-mode)
-
 
 (provide 'init-lsp-language)
 ;;; init-lsp-language.el ends here
--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -11,18 +11,13 @@
   (add-hook 'before-save-hook #'lsp-organize-imports t t)
   (add-hook 'before-save-hook #'lsp-format-buffer t t))
   
-
 (use-package lsp-mode
   :commands (lsp lsp-deferred lsp-format-buffer lsp-organize-imports)
+  :init (setq read-process-output-max (* 1024 1024) ; 1MB, data size read from server, default on 4K
+	      lsp-auto-guess-root t)
   :hook ((lsp-mode . lsp-enable-which-key-integration)
 	 (lsp-mode . lsp-lang-hooks)
-	 (prog-mode . (lambda()
-			(unless (derived-mode-p 'emacs-lisp-mode 'lisp-mode)
-			  (lsp-deferred)))))
-  :init
-  (setq lsp-auto-guess-root t
-        ;; 1MB, data size read from server, default on 4K
-        read-process-output-max (* 1024 1024)))
+	 (prog-mode . (lambda()(unless (derived-mode-p 'emacs-lisp-mode 'lisp-mode)(lsp-deferred))))))
   
 (use-package lsp-ui
   :after lsp-mode
@@ -30,8 +25,6 @@
   :hook ((lsp-mode . lsp-ui-mode)
          (lsp-ui-mode . lsp-modeline-code-actions-mode))
   :init (setq lsp-ui-doc-include-signature t
-              lsp-ui-doc-position 'at-point ;; top/bottom/at-point
-              lsp-ui-sideline-enable nil
               lsp-ui-sideline-ignore-duplicate t
               lsp-modeline-code-actions-segments '(count name)
               lsp-headerline-breadcrumb-enable nil)
@@ -40,7 +33,6 @@
   (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references))
 
 (use-package dap-mode
-  :diminish
   :hook ((lsp-mode . dap-mode)
          (dap-mode . dap-ui-mode)
 	 (dap-mode . dap-tooltip-mode)
