d8fffbb8cd308b38184e2d374e5e321c5cbf2368
cabins <kong_lingcun@foxmail.com>
2021-09-22 13:23:24 +0800

    init for v28 native

--- init.el
+++ init.el
@@ -3,31 +3,19 @@
 ;; Author: Cabins
 ;; Maintainer: Cabins
 ;; Version: 1.0
-;; Package-Requires: ()
 ;; Homepage: https://github.com/cabins
-;; Keywords:
-
-
-;; This file is not part of GNU Emacs
-
-;; This program is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;; (c) Cabins Kong, 2020-2021
 
 ;;; Code:
 (add-to-list 'load-path (expand-file-name (concat user-emacs-directory "lisp/")))
+(when (and (fboundp 'native-comp-available-p)
+	   (native-comp-available-p))
+  (setq native-comp-async-report-warnings-errors nil
+	comp-deferred-compilation t
+	package-native-compile t)
+  (add-to-list 'native-comp-eln-load-path (expand-file-name "eln-cache" user-emacs-directory)))
 
 ;; flymake cannot find load-path solution
 ;; [reference] https://emacs-china.org/t/flymake/8323/19
@@ -54,7 +42,6 @@
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (when (file-exists-p custom-file)
   (load-file custom-file))
-
 
 (provide 'init)
 
--- lisp/init-startup.el
+++ lisp/init-startup.el
@@ -11,20 +11,20 @@
 
 ;;; Code:
 
-;; 系统编码设置
+;; settings for system encoding
 (set-language-environment "UTF-8")
 (unless *is-windows*
   (setq selection-coding-system 'utf-8))
 
-(setq make-backup-files nil             ; 备份文件的设置
+(setq make-backup-files nil             ; disable backup file
       auto-save-default nil
-      inhibit-startup-screen t          ; 开屏动画与提示信息的配置
+      inhibit-startup-screen t          ; disable the startup screen splash
       inhibit-default-init t
       visible-bell nil
       inhibit-compacting-font-caches t
       read-process-output-max (* 64 1024))
 
-;; macOS删除文件的时候删除到垃圾桶
+;; macOS: move file to trash when delete
 (when *is-mac* (setq delete-by-moving-to-trash t))
 
 (provide 'init-startup)
